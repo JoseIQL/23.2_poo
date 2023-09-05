3490e4555cef5418e0e4b859103c1f1cb27c0601
JoseIQL <j.quisbertlopez@gmail.com>
2023-09-05 20:27:36 -0300

    refactor: move Cat class to house package

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -1,39 +1,7 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-abstract class Cat {
-//  ATRIBUTOS
-    private int age;
-    private int color;
-    private int energy;
-//  METODOS
-    public int getAge() {
-        return age;
-    }
+import house.Cat;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getColor() {
-        return color;
-    }
-
-    public void setColor(int color) {
-        this.color = color;
-    }
-    public abstract String jump();
-
-
-    public int getEnergy() {
-        return energy;
-    }
-
-    public void setEnergy(int energy) {
-        this.energy = energy;
-    }
-}
 // HERENCIA
-class SilentCat extends Cat{
+class SilentCat extends Cat {
         public String jump() {
             if (getEnergy() >= 10)
                 return "Tired";
