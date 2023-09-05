8079770752988442ba426b02103ebd9f15880424
JoseIQL <j.quisbertlopez@gmail.com>
2023-09-05 19:08:00 -0300

    feat: add abstract method jump

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -1,9 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-class Cat {
+abstract class Cat {
 //  ATRIBUTOS
     private int age;
     private int color;
+    private int energy;
 //  METODOS
     public int getAge() {
         return age;
@@ -20,27 +21,46 @@ class Cat {
     public void setColor(int color) {
         this.color = color;
     }
-    public String jump() {
-        return "juuumping";
-    }
-    
+    public abstract String jump();
 
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
 }
 // HERENCIA
 class SilentCat extends Cat{
+        public String jump() {
+            if (getEnergy() >= 10)
+                return "Tired";
+            setEnergy(getEnergy() + 1);
+            return "....";
+        }
+
+}
+class WildCat extends Cat{
     public String jump() {
-        return "....";
+        if (getEnergy() >=10)
+            return "Tired";
+        setEnergy(getEnergy() + 5);
+        return "rrrrrrrrr";
     }
 
 }
 
 public class Main {
     public static void main(String[] args) {
-        Cat miMichi = new Cat();
+        SilentCat miMichi = new SilentCat();
+        miMichi.setEnergy(1);
         miMichi.setAge(6);
         miMichi.setColor(1);
         System.out.println(miMichi.jump());
-
-
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
     }
 }
