ee47ce8e2fa5380b68a5cb25b8dc6629e0648930
JoseIQL <j.quisbertlopez@gmail.com>
2023-09-05 18:30:33 -0300

    feat: add SilentCat class

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -1,9 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 class Cat {
+//  ATRIBUTOS
     private int age;
     private int color;
-
+//  METODOS
     public int getAge() {
         return age;
     }
@@ -19,14 +20,27 @@ class Cat {
     public void setColor(int color) {
         this.color = color;
     }
-}
+    public String jump() {
+        return "juuumping";
+    }
+    
 
+}
+// HERENCIA
+class SilentCat extends Cat{
+    public String jump() {
+        return "....";
+    }
+
+}
 
 public class Main {
     public static void main(String[] args) {
         Cat miMichi = new Cat();
         miMichi.setAge(6);
         miMichi.setColor(1);
-        System.out.println(miMichi.getColor() + " " + miMichi.getAge());
+        System.out.println(miMichi.jump());
+
+
     }
 }
