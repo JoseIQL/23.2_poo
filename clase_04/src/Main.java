189111060297aa7ced9816ae8aa637e7b42992d7
JoseIQL <j.quisbertlopez@gmail.com>
2023-09-06 19:07:28 -0300

    feat: add Turtle class 04-09

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -1,12 +1,19 @@
 import house.Cat;
 import house.SilentCat;
+import house.Turtle;
 import house.WildCat;
 
 public class Main {
     public static void main(String[] args) {
         SilentCat bolaDeNieve = new SilentCat();
         WildCat miMichi = new WildCat();
+        Turtle donatelo = new Turtle();
 //bolaDeNieve y miMichi son los objetos.
+        donatelo.setName("Donatelo");
+        donatelo.setAge(50);
+        donatelo.setSpeed(5);
+        donatelo.setColor(3);
+
         bolaDeNieve.setEnergy(1);
         bolaDeNieve.setAge(6);
         bolaDeNieve.setColor(1);
@@ -25,6 +32,6 @@ public class Main {
 
         System.out.println(miMichi.getName() + " "+ miMichi.jump());
         System.out.println(bolaDeNieve.getName() + " " + bolaDeNieve.jump());
-
+        System.out.println(donatelo.getName() + " " + donatelo.swim());
     }
 }
---
+++ clase_04/src/house/Turtle.java
@@ -0,0 +1,16 @@
+package house;
+
+public class Turtle extends Animal{
+    private int speed;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public String swim(){
+        return "swimming";
+    }
+}
